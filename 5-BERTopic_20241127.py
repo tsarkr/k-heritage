298c6ddd6b0dbc6a1752b891441a31995e0f3d54
tsarkr <62873779+tsarkr@users.noreply.github.com>
2024-12-05 14:40:35 +0900

    Update 5-BERTopic_20241127.py

--- 5-BERTopic_20241127.py
+++ 5-BERTopic_20241127.py
@@ -2,7 +2,7 @@ import pandas as pd
 from bertopic import BERTopic
 
 # 파일 경로
-file_path = '/Users/gyungmin/VS_PRJ/DH/k-heritage/cleaned_youtube_comments_with_stopwords.csv'
+file_path = 'cleaned_youtube_comments_with_stopwords.csv'
 
 # 데이터 읽기
 data = pd.read_csv(file_path)
@@ -25,18 +25,18 @@ for idx, row in topic_info.head(10).iterrows():
     print(f"토픽 {topic_id}: {topic_name}")
 
 # 결과 저장 (토픽 정보 및 각 댓글의 토픽)
-topic_info.to_csv('/Users/gyungmin/VS_PRJ/DH/k-heritage/bertopic_topic_info.csv', index=False)
-print("\n토픽 정보 저장 완료: /Users/gyungmin/VS_PRJ/DH/k-heritage/bertopic_topic_info.csv")
+topic_info.to_csv('bertopic_topic_info.csv', index=False)
+print("\n토픽 정보 저장 완료: bertopic_topic_info.csv")
 
 # 각 댓글의 주요 토픽 저장
 comments_with_topics = pd.DataFrame({
     "Comment": comments,
     "Topic": topics
 })
-comments_with_topics.to_csv('/Users/gyungmin/VS_PRJ/DH/k-heritage/comments_with_topics.csv', index=False)
-print("\n댓글별 주요 토픽 저장 완료: /Users/gyungmin/VS_PRJ/DH/k-heritage/comments_with_topics.csv")
+comments_with_topics.to_csv('comments_with_topics.csv', index=False)
+print("\n댓글별 주요 토픽 저장 완료: comments_with_topics.csv")
 
 # 토픽 시각화 (HTML 파일 저장)
-visualization_path = '/Users/gyungmin/VS_PRJ/DH/k-heritage/bertopic_visualization.html'
+visualization_path = 'bertopic_visualization.html'
 topic_model.visualize_topics().write_html(visualization_path)
 print(f"\n토픽 시각화 저장 완료: {visualization_path}")
